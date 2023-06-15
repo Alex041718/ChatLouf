975d110a0c1ab55b16b650de8b953ac8c84462b6
Alexandre <Alexandre.alm18@gmail.com>
2023-06-15 16:16:24 +0200

    create files

---
+++ connectionPage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title></title>
+    </head>
+    <body>
+
+    
+    <form action="connection.php" method="post">
+        <p>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email"/>
+        </p>
+        <p>
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password"/>
+        </p>
+        <p>
+            <input type="submit" value="Se connecter"/>
+        </p>
+
+    </body>
+</html>
