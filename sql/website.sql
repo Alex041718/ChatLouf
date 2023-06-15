f80c1c16752a0cfe309281ca4bea8555a10fc556
DuncanC42 <cadoret.du@gmail.com>
2023-06-15 16:58:31 +0200

    Update website.sql

--- sql/website.sql
+++ sql/website.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS `WebSite`;
 CREATE DATABASE IF NOT EXISTS `WebSite`;
 USE `WebSite`;
 
@@ -7,5 +8,15 @@ CREATE TABLE `_User` (
     userName VARCHAR(255),
     password VARCHAR(255)
 );
+
+CREATE TABLE `_Message` (
+    messageID INT AUTO_INCREMENT PRIMARY KEY,
+    userID INT,
+    time DATETIME,
+    content VARCHAR(400),
+
+    CONSTRAINT message_fk_user FOREIGN KEY (userID) REFERENCES `_User`(userID)
+);
+
 
 insert into _User (mail, userName, password) values ('alexandre.alm18@gmail.com, alexalm35, alm');
